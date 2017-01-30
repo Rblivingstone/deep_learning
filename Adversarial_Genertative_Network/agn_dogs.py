6b214a76f7570a53da72de91839cfcd7c0a940bc
Ryan <ryan@barnesanalytics.com>
2017-01-30 09:17:17 -0700

    Some changes to agns
    Just some scripts to clean data....

--- Adversarial_Genertative_Network/agn_dogs.py
+++ Adversarial_Genertative_Network/agn_dogs.py
@@ -20,6 +20,7 @@ import numpy as np
 from PIL import Image
 import argparse
 import math
+from cleandata import Cleaner
 
 
 class adversarial_generative_network:
@@ -28,7 +29,7 @@ class adversarial_generative_network:
         self.g=self.make_generator_model()
         self.d=self.make_discriminator_model()
         self.agn=self.make_agn(self.g,self.d)
-        self.X=self.get_data()
+        self.X=Cleaner('h:\\Desktop\\GitHub\\practice_deep_learning\\deep_learning\\Adversarial_Genertative_Network\\Data\\train').build_X()
         return None
     
     def get_data(self):
@@ -38,15 +39,15 @@ class adversarial_generative_network:
         model = Sequential()
         model.add(Dense(input_dim=100, output_dim=1024))
         model.add(Activation('tanh'))
-        model.add(Dense(128*7*7))
+        model.add(Dense(56*7*7))
         model.add(BatchNormalization())
         model.add(Activation('tanh'))
-        model.add(Reshape((128, 7, 7), input_shape=(256*7*7,)))
+        model.add(Reshape((56, 7, 7), input_shape=(56*7*7,)))
         model.add(UpSampling2D(size=(2, 2)))
-        model.add(Convolution2D(128, 5, 5, border_mode='same'))
+        model.add(Convolution2D(56, 5, 5, border_mode='same'))
         model.add(Activation('tanh'))
-        model.add(UpSampling2D(size=(8, 8)))
-        model.add(Convolution2D(112, 5, 5, border_mode='same'))
+        model.add(UpSampling2D(size=(1, 8)))
+        model.add(Convolution2D(1, 5, 5, border_mode='same'))
         model.add(Activation('tanh'))
         return model
     
@@ -54,9 +55,9 @@ class adversarial_generative_network:
     def make_discriminator_model(self):
         model = Sequential()
         model.add(Convolution2D(
-                            64, 5, 5,
+                            1, 1, 112,
                             border_mode='same',
-                            input_shape=(1, 112, 112)))
+                            input_shape=(3, 112, 112)))
         model.add(Activation('tanh'))
         model.add(Flatten())
         model.add(Dense(1024))
@@ -69,8 +70,8 @@ class adversarial_generative_network:
     def make_agn(self,generator, discriminator):
         model = Sequential()
         model.add(generator)
-        #discriminator.trainable=False
-        #model.add(discriminator)
+        discriminator.trainable=False
+        model.add(discriminator)
         return model
     
     def combine_images(self,generated_images):
@@ -119,12 +120,17 @@ class adversarial_generative_network:
         self.d.compile(loss='binary_crossentropy',optimizer=d_optim)
         noise=np.zeros((BATCH_SIZE,100))
         for epoch in range(100):
-            for index in range(int(self.X/BATCH_SIZE)):
+            for index in range(int(len(self.X)/BATCH_SIZE)):
                 for i in range(BATCH_SIZE):
                     noise[i,:]=np.random.uniform(-1,1,100)
                 image_batch=self.X[index*BATCH_SIZE:(index+1)*BATCH_SIZE]
                 generated_images=self.g.predict(noise,verbose=0)
+                print(image_batch.shape)
+                print(generated_images.shape)
+                #image_batch=np.reshape(image_batch,(10,112,112))
+                generated_images=np.reshape(generated_images,(10,1,112,112))
                 X=np.concatenate((image_batch,generated_images))
+                #X=np.reshape(X,(20,1,112,112))
                 y=[1]*BATCH_SIZE+[0]*BATCH_SIZE
                 d_loss=self.d.train_on_batch(X,y)
                 for i in range(BATCH_SIZE):
@@ -136,4 +142,5 @@ class adversarial_generative_network:
         
 if __name__=='__main__':
     model=adversarial_generative_network()
+    model.train(10)
     model.generate_single_image().show()
